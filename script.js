ea0d40a4ca7ec45f975d58a9a02c44239a1d76cf
mark <122426238+dobladamark@users.noreply.github.com>
2025-11-11 21:57:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* mobile*/
+
+(function() {
+    const menuToggle = document.getElementsByClassName('btn-menu')[0];
+    const nav = document.getElementsByClassName('links-container')[0];
+
+    if (menuToggle && nav) {
+        menuToggle.addEventListener('click', () =>{
+            nav.classList.toggle('active1');
+        });
+    }
+})();
